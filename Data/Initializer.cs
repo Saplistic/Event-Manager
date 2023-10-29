00191f091accb48924748dc1ebfd87d8ec7c941c
Mat. Devski Carlier <carliermateo@outlook.be>
2023-10-29 12:05:29 +0100

    Add project files.

---
+++ Data/Initializer.cs
@@ -0,0 +1,27 @@
+﻿using Administration;
+using EventManager.Model;
+using Microsoft.EntityFrameworkCore.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Ribbon.Primitives;
+
+namespace EventManager.Data
+{
+    internal static class Initializer
+    {
+        internal static void DbSetInitializer(MyDBContext dbContext)
+        {
+            //if (!dbContext.MyModels.Any())
+            //{
+            //    dbContext.Add(new MyModel { Name = "model1" });
+            //    dbContext.Add(new MyModel { Name = "model2" });
+            //    dbContext.Add(new MyModel { Name = "model3" });
+
+            //    dbContext.SaveChanges();
+            //}
+        }
+    }
+}
---
+++ MyDBContext.cs
@@ -0,0 +1,25 @@
+﻿using EventManager.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Administration
+{
+    public class MyDBContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=localhost\\SQLEXPRESS;" +
+                "Database=EventManager;" +
+                "Trusted_Connection=True;" +
+                "MultipleActiveResultSets=True;" +
+                "TrustServerCertificate=True;");
+        }
+    }
+}
