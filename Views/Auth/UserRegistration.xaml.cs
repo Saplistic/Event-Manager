e4bab39442d54cada0f8a2549e84df13d7a84d8c
Mat. Devski Carlier <carliermateo@outlook.be>
2023-11-04 14:54:37 +0100

    Designed user login & registration forms

---
+++ Views/Auth/UserRegistration.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Administration;
+using EventManager.Data;
+using EventManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EventManager.Views.Auth
+{
+    /// <summary>
+    /// Interaction logic for UserRegistration.xaml
+    /// </summary>
+    public partial class UserRegistration : Window
+    {
+        public UserRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void SumbitRegistration(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
